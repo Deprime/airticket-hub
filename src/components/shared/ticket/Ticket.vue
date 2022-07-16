4bbd404d038168b5167489f6c730edf8298dc6a1
Andrey Dyugaev <a.dugaev@yandex.ru>
2022-07-17 03:20:22 +0500

    fix: logo hack for deployment

--- src/components/shared/ticket/Ticket.vue
+++ src/components/shared/ticket/Ticket.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
   import './Ticket.scss';
+
+  import S7Logo from '@/assets/images/companies/S7_Logo.png';
+  import XALogo from '@/assets/images/companies/XiamenAir_Logo.png';
+
   import { datetimeHelper, currencyHelper } from '@/helpers';
 
   // Components
@@ -11,6 +15,16 @@
     ticket: ITicket,
     company: ICompany
   }
+
+  interface ILogoMap {
+    [name: string]: string;
+  }
+
+  // Dirty hack
+  const logoMap: ILogoMap = {
+    "7dc12d0b-ce42-48a0-8673-0dad4d698764": XALogo,
+    "cddfa038-823b-43b1-b18d-395731881077": S7Logo,
+  };
 
   // Props
   const { ticket, company } = defineProps<Props>();
@@ -27,7 +41,7 @@
       </div>
       <div class="company-logo">
         <img
-          :src="logoSrc"
+          :src="logoMap[company.id]"
           :alt="company.name"
           class="inline-block"
           height="36p"
