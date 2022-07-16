d854a10b74c025964f4bec2f667cb24ba83e79e1
Andrey Dyugaev <a.dugaev@yandex.ru>
2022-07-17 03:09:04 +0500

    fix: github pages deploy fix for vite

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: process.env.NODE_ENV === 'production' ? "/airticket-hub/" : "",
   resolve: {
     alias: {
       // '@': path.resolve(__dirname, './src')
