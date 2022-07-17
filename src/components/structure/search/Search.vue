59ba415806400cce42886325f10cf0eeb093773a
Andrey Dyugaev <a.dugaev@yandex.ru>
2022-07-17 19:45:25 +0500

    feat: implement search section
    Input component added
    Search component markup added

---
+++ src/components/structure/search/Search.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  import './Search.scss';
+
+    // Components
+  import { Input } from '@/components/ui';
+</script>
+
+<template>
+  <section class="search">
+    <div class="at-control-group">
+      <Input native-type="text" placeholder="Откуда" />
+      <Input native-type="text" placeholder="Куда" />
+      <Input native-type="date" placeholder="Когда" />
+      <Input native-type="date" placeholder="Обратно" />
+    </div>
+  </section>
+</template>
