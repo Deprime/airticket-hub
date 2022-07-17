dfe526d5d791438587ea63f86b122fe3f6216a8d
Andrey Dyugaev <a.dugaev@yandex.ru>
2022-07-17 19:44:25 +0500

    feat: implement error alert component

---
+++ src/components/shared/error-alert/ErrorAlert.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+  import "./ErrorAlert.scss";
+
+  const props = defineProps({
+    title: {
+      type: String,
+      required: false,
+    },
+    details: {
+      type: String,
+      default: "Ошибка при загрузке данных",
+      required: false,
+    },
+  });
+</script>
+
+<template>
+  <div class="at-error-alert">
+    <h4 v-if="title">
+      {{ title }}
+    </h4>
+    <p>
+      {{ details }}
+    </p>
+  </div>
+</template>
