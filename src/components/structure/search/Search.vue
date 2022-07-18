94b2b57a826f11bdc3bc5c997818bbefd64e247d
Andrey Dyugaev <a.dugaev@yandex.ru>
2022-07-18 10:42:02 +0500

    feat: swap button

--- src/components/structure/search/Search.vue
+++ src/components/structure/search/Search.vue
@@ -3,6 +3,7 @@
   import { reactive } from 'vue';
 
   // Components
+  import SwapButton from './SwapButton.vue';
   import { Input } from '@/components/ui';
 
   // Data
@@ -12,12 +13,22 @@
     dateStart: "",
     dateEnd: "",
   });
+
+  // Methods
+  const onSwap = () => {
+    const departure = searchQuery.departure;
+    searchQuery.departure   = searchQuery.destination;
+    searchQuery.destination = departure;
+  }
 </script>
 
 <template>
   <section class="search">
     <div class="at-control-group">
       <Input native-type="text" v-model="searchQuery.departure" placeholder="Откуда" />
+      <SwapButton
+        @click="onSwap"
+      />
       <Input native-type="text" v-model="searchQuery.destination" placeholder="Куда" />
       <Input native-type="date" v-model="searchQuery.dateStart" placeholder="Когда" />
       <Input native-type="date" v-model="searchQuery.dateEnd" placeholder="Обратно" />
